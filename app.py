55b770515215891183ddbe0ca87986bf292a7ca9
AdmDvc <administrativo@dvc.net.br>
2026-05-28 16:57:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Meu Assistente Pessoal")
 
 # Cole a sua API Key entre as aspas abaixo
 genai.configure(api_key="AIzaSyDw0m23du2RWV8M_R6HBfgls5yhiKz4Abs")
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.0-pro')
 
 pergunta = st.text_input("Como posso ajudar?")
 if st.button("Enviar"):
